90d565ace8f9b0cc5d7200b3a1652d612e91bb6f
Assadullah Baig <assadullahbaig3@gmail.com>
2025-11-14 14:36:17 +0500

    first commit

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input.getAxis("Horizontal");
+        input.getAxis("Vertical");
+        
+    }
+}
